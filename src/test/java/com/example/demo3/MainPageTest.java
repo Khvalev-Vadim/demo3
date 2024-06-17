c3b73826863c9c45deddc26923b5fabe245936e0
Khvalev Vadim <j.khvalev.vadim@gmail.com>
2024-06-17 12:45:36 +0300

    add new driver

--- src/test/java/com/example/demo3/MainPageTest.java
+++ src/test/java/com/example/demo3/MainPageTest.java
@@ -22,10 +22,11 @@ public class MainPageTest {
 
         ChromeOptions options = new ChromeOptions();
         // Fix the issue https://github.com/SeleniumHQ/selenium/issues/11750
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        options.addArguments("--remote-allow-origins=*");
+//        driver = new ChromeDriver(options);
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = new ChromeDriver();
         driver.get("https://www.jetbrains.com/");
 
         mainPage = new MainPage(driver);
